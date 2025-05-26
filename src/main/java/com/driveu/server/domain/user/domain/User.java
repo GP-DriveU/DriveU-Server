196af57b2b2baa846d9968b249f16c3c2113d676
yeonjy73 <yeon703@naver.com>
2025-05-26 16:50:29 +0900

    feat: add user builder

--- src/main/java/com/driveu/server/domain/user/domain/User.java
+++ src/main/java/com/driveu/server/domain/user/domain/User.java
@@ -27,9 +27,31 @@ public class User {
     @Column(name = "email")
     private String email;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "o_auth_provider")
     private OauthProvider oauthProvider;
 
     @Column(name = "created_at")
     private LocalDateTime createdAt;
+
+    @Builder
+    private User(String name, String email, OauthProvider oauthProvider) {
+        this.name = name;
+        this.email = email;
+        this.oauthProvider = oauthProvider;
+    }
+
+    public static User of(String name, String email, OauthProvider oauthProvider) {
+        return User.builder()
+                .name(name)
+                .email(email)
+                .oauthProvider(oauthProvider)
+                .build();
+    }
+
+    // Entity 가 처음 저장될 때 자동으로 createdAt 필드에 현재 시각을 넣어줌
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
