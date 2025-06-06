4bb7f9381ac1ba0b5e4b48727499f8da0060ab30
yeonjy73 <yeon703@naver.com>
2025-06-06 17:36:22 +0900

    fix: exception fix

--- src/main/java/com/driveu/server/domain/summary/applicaion/SummaryService.java
+++ src/main/java/com/driveu/server/domain/summary/applicaion/SummaryService.java
@@ -1,5 +1,6 @@
 package com.driveu.server.domain.summary.applicaion;
 
+import com.amazonaws.services.kms.model.NotFoundException;
 import com.driveu.server.domain.resource.dao.NoteRepository;
 import com.driveu.server.domain.resource.domain.Note;
 import com.driveu.server.domain.summary.dao.SummaryRepository;
@@ -19,7 +20,7 @@ public class SummaryService {
     @Transactional
     public SummaryResponse createSummary(Long noteId) {
         Note note = noteRepository.findById(noteId)
-                .orElseThrow(() -> new IllegalArgumentException("Note not found"));
+                .orElseThrow(() -> new NotFoundException("Note not found"));
 
         // ai 서버 호출, 응답에서 summary 파싱
         String content = "test summary";
@@ -33,7 +34,7 @@ public class SummaryService {
     @Transactional
     public SummaryResponse getSummaryByNoteId(Long noteId) {
         Note note = noteRepository.findById(noteId)
-                .orElseThrow(() -> new IllegalArgumentException("Note not found"));
+                .orElseThrow(() -> new NotFoundException("Note not found"));
 
         Summary summary = summaryRepository.findByNote(note);
 
