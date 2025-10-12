6eabcd1e868401ba903b8d8f6575e0d106bd5977
yeonjy73 <yeon703@naver.com>
2025-10-12 16:45:24 +0900

    refactor: User entity with base entity

--- src/main/java/com/driveu/server/domain/user/domain/User.java
+++ src/main/java/com/driveu/server/domain/user/domain/User.java
@@ -1,22 +1,17 @@
 package com.driveu.server.domain.user.domain;
 
 import com.driveu.server.domain.auth.domain.oauth.OauthProvider;
+import com.driveu.server.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
-
-import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
 @Table(name = "user")
-public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = false)
-    private Long id;
+@Builder
+public class User extends BaseEntity {
 
     @Column(name = "name", nullable = false)
     private String name;
@@ -28,21 +23,13 @@ public class User {
     @Column(name = "o_auth_provider", nullable = false)
     private OauthProvider oauthProvider;
 
-    @Column(name = "created_at", updatable = false)
-    private LocalDateTime createdAt;
-
     @Column(name = "used_storage", nullable = false)
+    @Builder.Default
     private Long usedStorage = 0L; // 현재 사용 중인 바이트(Byte) 단위 저장 용량. 기본 0
 
     @Column(name = "max_storage", nullable = false, updatable = false)
+    @Builder.Default
     private Long maxStorage = 5368709120L;    // 이 유저에게 허용된 최대 저장 용량 (Byte 단위) -> 5GB
-
-    @Builder
-    private User(String name, String email, OauthProvider oauthProvider) {
-        this.name = name;
-        this.email = email;
-        this.oauthProvider = oauthProvider;
-    }
 
     public static User of(String name, String email, OauthProvider oauthProvider) {
         return User.builder()
@@ -50,12 +37,6 @@ public class User {
                 .email(email)
                 .oauthProvider(oauthProvider)
                 .build();
-    }
-
-    // Entity 가 처음 저장될 때 자동으로 createdAt 필드에 현재 시각을 넣어줌
-    @PrePersist
-    protected void onCreate() {
-        this.createdAt = LocalDateTime.now();
     }
 
     public void setUsedStorage(long updatedUsedStorage) {
