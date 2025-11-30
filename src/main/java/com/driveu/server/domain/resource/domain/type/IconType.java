b8dab3ece56f8fa8c85aa545c2f178985b169602
yeonjy73 <yeon703@naver.com>
2025-11-30 19:55:25 +0900

    feat: add linkType to default

--- src/main/java/com/driveu/server/domain/resource/domain/type/IconType.java
+++ src/main/java/com/driveu/server/domain/resource/domain/type/IconType.java
@@ -1,12 +1,15 @@
 package com.driveu.server.domain.resource.domain.type;
 
 public enum IconType {
-    YOUTUBE, GITHUB;
+    YOUTUBE, GITHUB, DEFAULT;
 
     public static IconType fromUrl(String url) {
         if (url.startsWith("https://github.com/")) {
             return IconType.GITHUB;
+        } else if (url.startsWith("https://www.youtube.com/")) {
+            return IconType.YOUTUBE;
+        } else {
+            return IconType.DEFAULT;
         }
-        return IconType.YOUTUBE;
     }
 }
