5859cb6d3616ba8f779ee68823bf181931cefa42
yeonjy73 <yeon703@naver.com>
2025-12-01 13:04:30 +0900

    fix: LinkSaveRequest dto 검증

--- src/main/java/com/driveu/server/domain/resource/dto/request/LinkSaveRequest.java
+++ src/main/java/com/driveu/server/domain/resource/dto/request/LinkSaveRequest.java
@@ -1,5 +1,6 @@
 package com.driveu.server.domain.resource.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,8 +11,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class LinkSaveRequest {
+
+    @NotBlank
     private String title;
+
+    @NotBlank
     private String url;
+
     private Long tagId; // nullable
 
     public LinkSaveRequest(String title, String url) {
