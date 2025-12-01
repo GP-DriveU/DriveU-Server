cd9fd38401c30a3e6ef4fb3fc7dc36a8a1f0e7c3
yeonjy73 <yeon703@naver.com>
2025-12-01 11:49:30 +0900

    fix: url 파라미터 null 체크

--- src/main/java/com/driveu/server/domain/resource/domain/type/IconType.java
+++ src/main/java/com/driveu/server/domain/resource/domain/type/IconType.java
@@ -4,6 +4,9 @@ public enum IconType {
     YOUTUBE, GITHUB, DEFAULT;
 
     public static IconType fromUrl(String url) {
+        if (url == null) {
+            throw new IllegalArgumentException("url cannot be null");
+        }
         if (url.startsWith("https://github.com/")) {
             return IconType.GITHUB;
         } else if (url.startsWith("https://www.youtube.com/")) {
