40bee98bab1e36f38462535e17da5f4762f1482b
yeonjy73 <yeon703@naver.com>
2026-03-04 22:52:55 +0900

    feat: delete aop

--- src/main/java/com/driveu/server/global/config/security/auth/OwnerVerifier.java
+++
@@ -1,7 +0,0 @@
-package com.driveu.server.global.config.security.auth;
-
-public interface OwnerVerifier<T> {
-    boolean supports(String resourceType);
-
-    boolean verify(Long resourceId, Long userId);
-}
